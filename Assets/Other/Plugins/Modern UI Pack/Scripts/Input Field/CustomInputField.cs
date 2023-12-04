bac464463d6863673f8abc3dda33bfa52f38cdb5
LukeGus <bugattiguy527@gmail.com>
2023-12-03 21:03:39 -0600

    Daily commit at Sun 12/03/2023 21:02:37.22

--- Assets/Other/Plugins/Modern UI Pack/Scripts/Input Field/CustomInputField.cs
+++ Assets/Other/Plugins/Modern UI Pack/Scripts/Input Field/CustomInputField.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using TMPro;
-#if !ENABLE_LEGACY_INPUT_MANAGER
+#if ENABLE_INPUT_SYSTEM
 using UnityEngine.InputSystem;
 #endif
 
-namespace Michsky.MUIP
+namespace Michsky.DreamOS
 {
     [RequireComponent(typeof(TMP_InputField))]
     [RequireComponent(typeof(Animator))]
@@ -19,96 +19,68 @@ namespace Michsky.MUIP
 
         [Header("Settings")]
         public bool processSubmit = false;
-        public bool clearOnSubmit = true;
 
         [Header("Events")]
         public UnityEvent onSubmit;
 
         // Hidden variables
-        private float cachedDuration = 0.5f;
         private string inAnim = "In";
         private string outAnim = "Out";
-        private string instaInAnim = "Instant In";
-        private string instaOutAnim = "Instant Out";
-        private bool isActive = false;
 
         void Awake()
         {
-            Initialize();
+            if (inputText == null) { inputText = gameObject.GetComponent<TMP_InputField>(); }
+            if (inputFieldAnimator == null) { inputFieldAnimator = gameObject.GetComponent<Animator>(); }
 
-            inputText.onSelect.AddListener(delegate { AnimateIn(); });
-            inputText.onEndEdit.AddListener(delegate { HandleEndEdit(); });
             inputText.onValueChanged.AddListener(delegate { UpdateState(); });
-
-            UpdateStateInstant();
+            inputText.onSelect.AddListener(delegate { AnimateIn(); });
+            inputText.onEndEdit.AddListener(delegate { AnimateOut(); });
         }
 
         void OnEnable()
         {
-            if (inputText == null || inputFieldAnimator == null) { Initialize(); }
+            if (inputText == null)
+                return;
+
             inputText.ForceLabelUpdate();
-            UpdateStateInstant();
+            UpdateState();
+
+            if (gameObject.activeInHierarchy == true) { StartCoroutine("DisableAnimator"); }
         }
 
         void Update()
         {
-            if (!processSubmit ||string.IsNullOrEmpty(inputText.text) ||  EventSystem.current.currentSelectedGameObject != inputText.gameObject)
+            if (processSubmit == false 
+                || string.IsNullOrEmpty(inputText.text) == true 
+                || EventSystem.current.currentSelectedGameObject != inputText.gameObject)
                 return;
 
 #if ENABLE_LEGACY_INPUT_MANAGER
-            if (Input.GetKeyDown(KeyCode.Return)) 
-            { 
-                onSubmit.Invoke();
-
-                if (clearOnSubmit) 
-                {
-                    inputText.text = ""; 
-                    UpdateState();
-                } 
-            }
+            if (Input.GetKeyDown(KeyCode.Return)) { onSubmit.Invoke(); inputText.text = ""; }
 #elif ENABLE_INPUT_SYSTEM
-            if (Keyboard.current.enterKey.wasPressedThisFrame) 
-            { 
-                onSubmit.Invoke(); 
-                
-                if (clearOnSubmit) 
-                { 
-                    inputText.text = ""; 
-                    UpdateState();
-                } 
-            }
+            if (Keyboard.current.enterKey.wasPressedThisFrame) { onSubmit.Invoke(); }
 #endif
         }
 
-        void Initialize()
+        public void AnimateIn()
         {
-            if (inputText == null) { inputText = gameObject.GetComponent<TMP_InputField>(); }
-            if (inputFieldAnimator == null) { inputFieldAnimator = gameObject.GetComponent<Animator>(); }
-        }
+            StopCoroutine("DisableAnimator");
 
-        public void AnimateIn() 
-        {
-            if (inputFieldAnimator.gameObject.activeInHierarchy && !isActive) 
+            if (inputFieldAnimator.gameObject.activeInHierarchy == true)
             {
-                StopCoroutine("DisableAnimator");
-                StartCoroutine("DisableAnimator");
-
-                isActive = true;
                 inputFieldAnimator.enabled = true;
                 inputFieldAnimator.Play(inAnim);
+                StartCoroutine("DisableAnimator");
             }
         }
 
         public void AnimateOut()
         {
-            if (inputFieldAnimator.gameObject.activeInHierarchy && inputText.text.Length == 0 && isActive)
+            if (inputFieldAnimator.gameObject.activeInHierarchy == true)
             {
-                StopCoroutine("DisableAnimator");
-                StartCoroutine("DisableAnimator");
-
-                isActive = false;
                 inputFieldAnimator.enabled = true;
-                inputFieldAnimator.Play(outAnim);
+                if (inputText.text.Length == 0) { inputFieldAnimator.Play(outAnim); }
+                StartCoroutine("DisableAnimator");
             }
         }
 
@@ -118,30 +90,9 @@ namespace Michsky.MUIP
             else { AnimateIn(); }
         }
 
-        public void UpdateStateInstant()
-        {
-            inputFieldAnimator.enabled = true;
-
-            StopCoroutine("DisableAnimator");
-            StartCoroutine("DisableAnimator");
-
-            if (inputText.text.Length == 0) { isActive = false; inputFieldAnimator.Play(instaOutAnim);  }
-            else { isActive = true; inputFieldAnimator.Play(instaInAnim); }
-        }
-
-        void HandleEndEdit()
-        {
-            if (string.IsNullOrEmpty(inputText.text) && !EventSystem.current.alreadySelecting && EventSystem.current.currentSelectedGameObject == inputText.gameObject)
-            {
-                EventSystem.current.SetSelectedGameObject(null);
-            }
-
-            AnimateOut();
-        }
-
         IEnumerator DisableAnimator()
         {
-            yield return new WaitForSecondsRealtime(cachedDuration);
+            yield return new WaitForSeconds(1);
             inputFieldAnimator.enabled = false;
         }
     }
