5414e27cc5b57f59bec4b76941d72a8dd1d418b7
LukeGus <bugattiguy527@gmail.com>
2024-01-01 21:00:07 -0600

    Daily commit at Mon 01/01/2024 21:00:07.28

---
+++ Assets/SpawnManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [Header("Animatronic Prefabs")]
+    public GameObject animatronic1GameObject;
+    public GameObject animatronic2GameObject;
+    public GameObject animatronic3GameObject;
+    public GameObject animatronic4GameObject;
+    public GameObject animatronic5GameObject;
+    
+    [Header("Animatronic Spawn Points")]
+    public GameObject animatronic1SpawnPoint;
+    public GameObject animatronic2SpawnPoint;
+    public GameObject animatronic3SpawnPoint;
+    public GameObject animatronic4SpawnPoint;
+    public GameObject animatronic5SpawnPoint;
+    
+    [Header("Gaurd")]
+    public GameObject gaurd;
+    
+    [Header("Gaurd Spawn Point")]
+    public GameObject gaurdlSpawnPoint;
+    
+    void Start()
+    {
+        
+    }
+}
