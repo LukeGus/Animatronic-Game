4e098e9e56bd550a7d0615cb78c5ba74fa1308a8
LukeGus <bugattiguy527@gmail.com>
2024-02-08 21:00:05 -0600

    Daily commit at Thu 02/08/2024 21:00:05.40

--- Assets/Scripts and Assets/Dependencies/GameManagers/NetworkDisabler.cs
+++ Assets/Scripts and Assets/Dependencies/GameManagers/NetworkDisabler.cs
@@ -4,11 +4,17 @@ using Unity.Netcode;
 public class NetworkDisabler : NetworkBehaviour
 {
     [SerializeField] private GameObject cameraHolder;
+    [SerializeField] private EventSystem eventSystem;
     
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
         
         cameraHolder.SetActive(IsOwner);
+        
+        if (!IsOwner)
+        {
+            Destroy(eventSystem);
+        }
     }
 }
